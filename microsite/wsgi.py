609844d47981bf04fd0e8a2721cece55e3def6aa
KripanKhadka <ashketchium83@gmail.com>
2026-08-21 23:01:45 +0545

    Add app alias for Vercel WSGI entry point

--- microsite/wsgi.py
+++ microsite/wsgi.py
@@ -6,4 +6,8 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "microsite.settings")
 
 # if socket.gethostbyname(socket.gethostname()) == "136.243.86.126":
 #     import newrelic.agent
+
 application = get_wsgi_application()
+
+# Alias for Vercel WSGI resolution
+app = application
